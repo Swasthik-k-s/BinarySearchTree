28caedbd995247bcb6666f2d94a984be819fdca9
Swasthik <swasthik.ks22@gmail.com>
2021-09-15 23:04:25 +0530

    Added Size Method

--- src/com/bst/MyBinaryTreeMain.java
+++ src/com/bst/MyBinaryTreeMain.java
@@ -7,6 +7,17 @@ public class MyBinaryTreeMain {
 		myBinaryTree.add(56);
 		myBinaryTree.add(30);
 		myBinaryTree.add(70);
+		myBinaryTree.add(22);
+		myBinaryTree.add(40);
+		myBinaryTree.add(11);
+		myBinaryTree.add(3);
+		myBinaryTree.add(16);
+		myBinaryTree.add(60);
+		myBinaryTree.add(95);
+		myBinaryTree.add(65);
+		myBinaryTree.add(63);
+		myBinaryTree.add(67);
+		System.out.println("Size of the Binary Search Tree is " + myBinaryTree.getSize());
 		myBinaryTree.print();
 	}
 
